93ac6784859dfd6028b1f9a79fa995557b6413ba
Supermagzzz <30972311+Supermagzzz@users.noreply.github.com>
2020-02-16 00:54:22 +0300

    Update database.js

--- a.ru/www/js/database.js
+++ a.ru/www/js/database.js
@@ -17,3 +17,15 @@ function search() {
 	document.getElementById("spinner").style.visibility = "visible";
 	setMarker(a);
 }
+
+function getPath(a, b) {
+	var xhttp;
+	xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			this.responseText;
+		}
+	};	
+	xhttp.open("GET", "php/findPath.php?start=" + a + "&end=" + b, true);
+	xhttp.send();
+}
