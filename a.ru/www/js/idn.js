43c33cb110f1ba4f5ea03c4c5f4cade5351358d6
flydzen <flydzen@users.noreply.github.com>
2020-02-15 22:24:52 +0300

    3

---
+++ a.ru/www/js/idn.js
@@ -0,0 +1,3 @@
+function printMarker(x, y,){
+    
+}
---
+++ a.ru/www/js/points.js
@@ -0,0 +1,13 @@
+class Point {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  
+    print() {
+      let point = document.createElement("img");
+      point.src = "../img/Circle-icon.png";
+    }
+  
+  }
+  
