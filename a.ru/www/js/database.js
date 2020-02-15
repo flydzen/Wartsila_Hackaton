b7f21d4f68b3ef248ebd173618aaf0f0311d7ae7
flydzen <flydzen@users.noreply.github.com>
2020-02-16 00:52:31 +0300

    notshit

--- a.ru/www/js/database.js
+++ a.ru/www/js/database.js
@@ -5,8 +5,15 @@ function setMarker(name) {
 		if (this.readyState == 4 && this.status == 200) {
 			var arr = this.responseText.split(' ');
 			marker.move(+arr[0], +arr[1]);
+			document.getElementById("spinner").style.visibility = "hidden";
 		}
-	};
+	};	
 	xhttp.open("GET", "php/getCoords.php?room=" + name, true);
 	xhttp.send();
+}
+
+function search() {
+	var a = document.getElementById("roomNum").value;
+	document.getElementById("spinner").style.visibility = "visible";
+	setMarker(a);
 }
