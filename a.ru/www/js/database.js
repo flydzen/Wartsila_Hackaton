0e48b32c1f761f62cda56821e5a11216b0ddb8aa
flydzen <flydzen@users.noreply.github.com>
2020-02-16 03:17:51 +0300

    spinners and marker

--- a.ru/www/js/database.js
+++ a.ru/www/js/database.js
@@ -19,6 +19,7 @@ function search() {
 }
 
 function getPath() {
+	document.getElementById("spinner").style.visibility = "visible";
 	var from = document.getElementById("roomNumFrom").value;
 	var to = document.getElementById("roomNumTo").value;
 	var xhttp;
@@ -31,21 +32,15 @@ function getPath() {
 			} catch (Exception) {}
 			path = draw.polyline(this.responseText).fill('none');
 			path.stroke({ color: '#f06', width: 4, linecap: 'round', linejoin: 'round' })
+			document.getElementById("spinner").style.visibility = "hidden";
 		}
 	};	
 	xhttp.open("GET", "php/findPath.php?start=" + from + "&end=" + to, true);
 	xhttp.send();
 }
 
-function alert_coords(evt) {
-    pt.x = evt.clientX;
-    pt.y = evt.clientY;
-
-    var cursorpt =  pt.matrixTransform(svg.getScreenCTM().inverse());
-    console.log("(" + cursorpt.x + ", " + cursorpt.y + ")");
-}
-
 function getNearRoom(x, y, floor) {
+	document.getElementById("spinner").style.visibility = "visible";
 	var xhttp;
 	xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
