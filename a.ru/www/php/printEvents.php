534a2c68edc4c9a11b766c988e341d9413dd353d
Supermagzzz <30972311+Supermagzzz@users.noreply.github.com>
2020-02-16 11:31:47 +0300

    Create printEvents.php

---
+++ a.ru/www/php/printEvents.php
@@ -0,0 +1,8 @@
+<?php
+    header('Content-type: text/html; charset=utf-8');
+	$mysqli = new mysqli("localhost", "root", "", "nav");
+	$result = $mysqli->query("SELECT `id`, `name`, `time` FROM events");
+    while ($row = $result->fetch_assoc()) {
+        echo '<button type="button" value="'.$row['id'].'" class="list-group-item list-group-item-action">'.$row['name'].'</button>';
+    }
+?>
