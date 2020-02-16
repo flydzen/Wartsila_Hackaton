2619d5bad5d4899a090988d65c257889010497f1
flydzen <flydzen@users.noreply.github.com>
2020-02-16 08:32:41 +0300

    1231

--- a.ru/www/index.php
+++ a.ru/www/index.php
@@ -62,9 +62,7 @@
           }
       });
       svgBase = document.getElementById('drawing');
-      svgBase.onmousedown = function(e) {
-        svgBase.transform='translate('+e.x + ', ' + e.y +')'
-      }
+      makeDraggable(svgBase);
     }
     var startCircle;
     var endCircle;
